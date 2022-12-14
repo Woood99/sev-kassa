0685bc19cc6becc4900971f73fdc9746310d122e
Sergey Stryuchkov <serega.stryuchkov@gmail.com>
2022-12-14 14:34:51 +0300

    finished header and hero

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -24,7 +24,7 @@
 
 
 // Реализация бургер-меню
-// import { burger } from './functions/burger';
+import { burger } from './functions/burger';
 
 
 // ========================================================================================
@@ -57,9 +57,13 @@
 
 
 // Получение высоты шапки сайта (не забудьте вызвать функцию)
-// import { getHeaderHeight } from './functions/header-height';
-// getHeaderHeight();
-
+import { getHeaderHeight, getTopInfoHeight } from './functions/get-block-height';
+getHeaderHeight();
+getTopInfoHeight();
+window.addEventListener('resize',() => {
+    getHeaderHeight();
+    getTopInfoHeight();
+});
 
 // ========================================================================================
 
@@ -188,14 +192,14 @@
 
 
 /* Динамический адаптив */
-// import "./functions/dynamic-adapt";
+import "./functions/dynamic-adapt";
 
 
 // ========================================================================================
 
 
 // Фикс фулскрин-блоков
-// import './functions/fix-fullheight';
+import './functions/fix-fullheight';
 
 
 // ========================================================================================
--- src/js/functions/get-block-height.js
+++ src/js/functions/get-block-height.js
@@ -2,3 +2,8 @@ export const getHeaderHeight = () => {
   const headerHeight = document?.querySelector('.header').offsetHeight;
   document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 }
+
+export const getTopInfoHeight = () => {
+  const topInfo = document?.querySelector('.top-info').offsetHeight;
+  document.querySelector(':root').style.setProperty('--top-info-height', `${topInfo}px`);
+}
